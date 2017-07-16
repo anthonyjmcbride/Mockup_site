9d80aaa451102c456c643c5f7f3916e736215704
“Anthony <anthonyjmcbride@icloud.com>
2017-07-16 15:17:04 -0400

    Added price to Posts/Admin boolean to Admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170715065658) do
+ActiveRecord::Schema.define(version: 20170716191550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170715065658) do
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.boolean "admin"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -36,6 +37,15 @@ ActiveRecord::Schema.define(version: 20170715065658) do
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.integer "price"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
