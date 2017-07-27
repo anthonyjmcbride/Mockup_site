ab93785a6577dfd0638633f726dee72ac951806b
“Anthony <anthonyjmcbride@icloud.com>
2017-07-27 17:40:19 -0400

    Fixed show error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authorize,  only: [:create, :show, :edit, :update, :destroy]
+  before_action :set_post, only: [:show]
 
   # GET /posts
   # GET /posts.json
@@ -74,6 +75,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :description, :avatar)
+      params.require(:post).permit(:title, :description, :avatar, :price)
     end
 end
