15fc0fac04ee9beb46d61dbabbe6c72859da5abd
“Anthony <anthonyjmcbride@icloud.com>
2017-09-24 19:06:55 -0400

    catch up commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
 
   has_attached_file :avatar, styles: {
      medium: "1000x1000<",
-     thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+     thumb: "500x500>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
 end
